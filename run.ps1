621b43defad04057ea3d119f0d4ed9cd9cdf50a3
Mike Martinescu <mikey.x88@gmail.com>
2024-05-24 21:58:55 -0700

    Add comments

--- run.ps1
+++ run.ps1
@@ -1,5 +1,7 @@
 cls
 
+# NOTE: assumes you have an environment names .venv
+# TODO: check for environment; create it if it doesn't exist
 .venv\Scripts\activate
 
 # TODO: add arguments
--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 clear
 
 # NOTE: assumes you have an environment names .venv
+# TODO: check for environment; create it if it doesn't exist
 ./.venv/bin/activate
 
 # TODO: add arguments
